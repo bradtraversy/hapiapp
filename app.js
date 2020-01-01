5879bc9afd14352d1e50f668e368a5f2c528979c
Damian <damianjnc@gmail.com>
2020-01-01 05:24:38 +0000

    updates

--- app.js
+++ app.js
@@ -1,123 +1,104 @@
-const Hapi = require('hapi');
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/hapidb', { useMongoClient: true })
-    .then(() => console.log('MongoDB connected...'))
-    .catch(err => console.error(err));
+const Hapi = require('hapi')
+const mongoose = require('mongoose')
+mongoose
+  .connect('mongodb://localhost/hapidb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('mongo db connected'))
+  .catch(err => console.log(err))
 
-// Create Task Model
-const Task = mongoose.model('Task', {text:String});
+//create task model
+const Task = mongoose.model('Task', { text: String })
 
-// Init Server
-const server = new Hapi.Server();
-
-// Add Connection
-server.connection({
+const init = async () => {
+  //Init server
+  const server = new Hapi.server({
     port: 8000,
-    host:'localhost'
-});
+    host: 'localhost'
+  })
 
-// Home Route
-server.route({
-    method:'GET',
-    path:'/',
-    handler: (request, reply) => {
-        //reply('<h1>Hello World</h1>');
-        reply.view('index',{
-            name:'John Doe'
-        });
-    }
-});
+  await server.register(require('inert'))
+  await server.register(require('vision'))
 
-// Dynamic Route
-server.route({
-    method:'GET',
-    path:'/user/{name}',
-    handler: (request, reply) => {
-        reply('Hello, '+request.params.name);
-    }
-});
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (req, h) => h.view('index', { name: 'Damianek' })
+  })
 
-// GET Tasks Route
-server.route({
-    method:'GET',
-    path:'/tasks',
-    handler: (request, reply) => {
-        let tasks = Task.find((err, tasks) => {
-            //console.log(tasks);
-            reply.view('tasks', {
-                tasks:tasks
-            });
-        });
-        /*
-        reply.view('tasks',{
-            tasks:[
-                {text:'Task One'},
-                {text:'Task Two'},
-                {text:'Task Three'}
-            ]
-        });
-        */
-    }
-});
-
-// POST Tasks Route
-server.route({
-    method:'POST',
-    path:'/tasks',
-    handler: (request, reply) => {
-        let text = request.payload.text;
-        let newTask= new Task({text:text});
-        newTask.save((err, task) => {
-            if(err) return console.log(err);
-            return reply.redirect().location('tasks');
-        });
-    }
-});
-
-
-// Static Routes
-server.register(require('inert'), (err) => {
-    if(err){
-        throw err;
+  //Get task route
+  server.route({
+    method: 'GET',
+    path: '/tasks',
+    handler: async (req, h) => {
+      let tasks = await Task.find((err, tasks) => {
+        console.log(tasks)
+      })
+      return h.view('tasks', {
+        tasks: tasks
+      })
     }
 
-    server.route({
-        method:'GET',
-        path:'/about',
-        handler: (request, reply) => {
-            reply.file('./public/about.html');
-        }
-    });
+    //   h.view('tasks', {
+    //     tasks: [
+    //       { text: 'task one' },
+    //       { text: 'task two' },
+    //       { text: 'task three' }
+    //     ]
+    //   })
+  })
 
-    server.route({
-        method:'GET',
-        path:'/image',
-        handler: (request, reply) => {
-            reply.file('./public/hapi.png');
-        }
-    });
-});
+  //Post task route
+  server.route({
+    method: 'POST',
+    path: '/tasks',
+    handler: async (req, h) => {
+      let text = req.payload.text
+      let newTask = new Task({ text: text })
+      await newTask.save((err, task) => {
+        if (err) return console.log(err)
+      })
 
-// Vision Templates
-server.register(require('vision'), (err) => {
-    if(err){
-        throw err;
+      return h.redirect().location('tasks')
     }
+  })
 
-    server.views({
-        engines: {
-            html:require('handlebars')
-        },
-        path: __dirname + '/views'
-    });
-});
-
-
-// Start Server
-server.start((err) => {
-    if(err){
-        throw err;
+  server.route({
+    method: 'GET',
+    path: '/user/{name}',
+    handler: (req, h) => {
+      return `Hello world, ${req.params.name}`
     }
+  })
 
-    console.log(`Server started at: ${server.info.uri}`);
-});
+  server.route({
+    method: 'GET',
+    path: '/about',
+    handler: (req, h) => {
+      return h.file('./public/about.html')
+    }
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/image',
+    handler: (req, h) => {
+      return h.file('./public/hapijs.jpeg')
+    }
+  })
+
+  server.views({
+    engines: {
+      html: require('handlebars')
+    },
+    relativeTo: __dirname,
+    path: 'views'
+  })
+
+  await server.start()
+
+  console.log(`Server is running on ${server.info.uri}`)
+}
+
+init().catch(err => console.log(err))
